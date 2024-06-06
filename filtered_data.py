9ab966a85deb8df2b6c30aa678957bc40f636b9c
TolsonA <drewtol@yahoo.com>
2024-06-06 18:22:18 -0400

    last save for the day

--- filtered_data.py
+++ filtered_data.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
 
 df_data = pd.read_csv('data.csv')
+df_data = df_data.drop(columns=['Unnamed: 60', 'Country Code', '2015'])
 
 df_france = df_data[df_data['Country Name'].str.contains('France')]
 df_japan = df_data[df_data['Country Name'].str.contains("Japan")]
