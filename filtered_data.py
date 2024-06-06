678a9b1cb8c81da17d7aa36248b0defd62fcd4ae
Austin Heard <amheard0311@outlook.com>
2024-06-06 14:30:17 -0600

    Adds data folder for .csv, temp adds in austin's jupyter playgroud

--- filtered_data.py
+++ filtered_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df_data = pd.read_csv('data.csv')
+df_data = pd.read_csv('data/data.csv')
 
 df_france = df_data[df_data['Country Name'].str.contains('France')]
 df_japan = df_data[df_data['Country Name'].str.contains("Japan")]
